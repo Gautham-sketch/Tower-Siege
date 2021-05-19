56a452b29c10f2229dde595ffeb350db835def76
Gautham-sketch <82305202+Gautham-sketch@users.noreply.github.com>
2021-05-19 12:55:01 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,21 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+                     
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+        this.image = loadImage("block.png");
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        image(this.image,pos.x,pos.y,40,40);
+        pop();
+      }
+}
